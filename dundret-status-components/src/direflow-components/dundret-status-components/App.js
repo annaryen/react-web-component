5ed269fc75657807ff9895c69ef22f38b5d35f63
Anna Ryen <annaryen@hotmail.se>
2021-11-29 16:17:45 +0100

    After fetching data
    fetched all data and created some folders

--- dundret-status-components/src/direflow-components/dundret-status-components/App.js
+++ dundret-status-components/src/direflow-components/dundret-status-components/App.js
@@ -1,52 +1,60 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { EventContext, Styled } from 'direflow-component';
+import React from 'react';
+import { Styled } from 'direflow-component';
 import styles from './App.css';
+import Status from './Logic/Status';
 
-const App = (props) => {
-  const dispatch = useContext(EventContext);
+const App = () => {
+//   const dispatch = useContext(EventContext);
 
-  const handleClick = () => {
-    const event = new Event('my-event');
-    dispatch(event);
-  };
+//   const handleClick = () => {
+//     const event = new Event('my-event');
+//     dispatch(event);
+//   };
 
-  const renderSampleList = props.sampleList.map((sample) => (
-    <div key={sample} className='sample-text'>
-      → {sample}
-    </div>
-  ));
+//   const renderSampleList = props.sampleList.map((sample) => (
+//     <div key={sample} className='sample-text'>
+//       → {sample}
+//     </div>
+//   ));
 
   return (
     <Styled styles={styles}>
       <div className='app'>
-        <div className='top'>
-          <div className='header-image' />
-        </div>
-        <div className='bottom'>
-          <div className='header-title'>{props.componentTitle}</div>
-          <div>{renderSampleList}</div>
-          <button className='button' onClick={handleClick}>
-            Click me!
-          </button>
-        </div>
+        <Status />
       </div>
     </Styled>
   );
 };
 
-App.defaultProps = {
-  componentTitle: 'Dundret Status Components',
-  sampleList: [
-    'Create with React',
-    'Build as Web Component',
-    'Use it anywhere!',
-  ],
-}
 
-App.propTypes = {
-  componentTitle: PropTypes.string,
-  sampleList: PropTypes.array,
-};
+{/* 
+  import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { EventContext, Styled } from 'direflow-component';
+import styles from './App.css';
+  
+  <div className='top'>
+<div className='header-image' />
+</div>
+<div className='bottom'>
+<div className='header-title'>{props.componentTitle}</div>
+<div>{renderSampleList}</div>
+<button className='button' onClick={handleClick}>
+  Click me!
+</button>
+</div> */}
+// App.defaultProps = {
+//   componentTitle: 'Dundret Status Components',
+//   sampleList: [
+//     'Create with React',
+//     'Build as Web Component',
+//     'Use it anywhere!',
+//   ],
+// }
+
+// App.propTypes = {
+//   componentTitle: PropTypes.string,
+//   sampleList: PropTypes.array,
+// };
 
 export default App;

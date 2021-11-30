c3d365d5368cd979fc526cd5699198eb3c31244d
Anna Ryen <annaryen@hotmail.se>
2021-11-30 18:09:17 +0100

    Status dots in place
    drift status dots working

--- dundret-status-components/src/direflow-components/dundret-idag/Logic/Status.js
+++ dundret-status-components/src/direflow-components/dundret-idag/Logic/Status.js
@@ -3,11 +3,21 @@ import Liftar from '../Presentation/Liftar';
 import Weather from '../Presentation/Weather';
 
 function Status() {
+  // Drift status OPEN for lifts
   const [barnLift, setBarnLift] = useState();
   const [expr, setExpr] = useState();
   const [fjl, setFjl] = useState();
   const [mellanLift, setMellanLift] = useState();
   const [liikavaara, setLiikavaara] = useState();
+
+  // Drift status READY for lifts
+  const [barnReady, setBarnReady] = useState();
+  const [exprReady, setExprReady] = useState();
+  const [fjlReady, setFjlReady] = useState();
+  const [mellanReady, setMellanReady] = useState();
+  const [liikavaaraReady, setLiikavaaraReady] = useState();
+
+  // Weather
   const [temp, setTemp] = useState();
   const [coldEffect, setColdEffect] = useState();
   const [windSpeed, setWindSpeed] = useState();
@@ -26,14 +36,22 @@ function Status() {
 
       const data = await response.json();
       console.log(data);
+      // Lift drift
       setBarnLift(data.BB_DRIFT.Value);
       setExpr(data.EX_DRIFT.Value);
       setFjl(data.FJL_DRIFT.Value);
       setMellanLift(data.ML_DRIFT.Value);
       setLiikavaara(data.SL_DRIFT.Value);
+      // Weahter
       setTemp(data.TT_DAL_PV.Value);
       setColdEffect(data.WC_DAL_PV.Value);
       setWindSpeed(data.WS_DAL_PV.Value);
+      // Lift Ready
+      setBarnReady(data.BB_STARTKLAR.Value);
+      setExprReady(data.EX_STARTKLAR.Value);
+      setFjlReady(data.FJL_STARTKLAR.Value);
+      setMellanReady(data.ML_STARTKLAR.Value);
+      setLiikavaaraReady(data.SL_STARTKLAR.Value);
       
 
     } catch (error) {
@@ -50,7 +68,13 @@ function Status() {
         fjl={fjl}
         mellanLift={mellanLift}
         liikavaara={liikavaara}
+        barnReady={barnReady}
+        exprReady={exprReady}
+        fjlReady={fjlReady}
+        mellanReady={mellanReady}
+        liikavaaraReady={liikavaaraReady}
       />
+      
       <Weather 
         temp={temp}
         coldEffect={coldEffect}
